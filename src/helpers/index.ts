fafbd117c960322fd85408c78cfd4826448c6eb7
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2022-05-15 01:54:29 +0200

    Adding comments in hard to understand parts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -36,13 +36,19 @@ const checkVertically = (board: Board) => {
 
 const checkDiagonals = (board: Board) => {
   const size = board?.length;
+  //create 1d array
   const flatBoard = Array.prototype.concat.apply([], board);
   const firstDiagonal = [];
   const secondDiagonal = [];
 
   for (let i = 0; i < size; i++) {
+    /*pattern to find first diagonal item for each row
+     where row number is i */
     const firstDiagonalPattern = i * (1 + size);
+    /*pattern to find second diagonal item for each row
+     where row number is i */
     const secondDiagonalPattern = (size - 1) * (i + 1);
+    //pushing the finded value for that row to the diagonals arrays
     firstDiagonal?.push(flatBoard[firstDiagonalPattern]);
     secondDiagonal?.push(flatBoard[secondDiagonalPattern]);
   }
