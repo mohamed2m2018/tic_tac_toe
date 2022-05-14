5076401938fc02f15aa6c9400ba53771f66a9223
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2022-05-15 00:02:44 +0200

    Initialize app

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import GameBoard from './src/components/GameBoard';
+
+const App = () => (
+  <>
+    <Text style={styles.header}>Tic Tac Toe</Text>
+    <View style={styles.container}>
+      <GameBoard />
+    </View>
+  </>
+);
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  header: {
+    fontSize: 40,
+    alignSelf: 'center',
+    marginTop: 50,
+    marginBottom: 150,
+  },
+});
